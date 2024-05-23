b7f8d1ee0c2f1f67e1b3446406e29da2cae47419
icanci <1845666903@qq.com>
2024-05-23 09:40:43 +0800

    rpc interface

--- ethers-simple-storage-fcc/depoly.js
+++ ethers-simple-storage-fcc/depoly.js
@@ -1,11 +1,14 @@
 async function main() {
-  // 我们可以运行js通过 node deploy.js
-  console.log("hi");
-
-  let variable = 5;
-
-  console.log(variable);
+  // 编译合约代码
+  // yarn solcjs --bin --abi --include-path node_modules/ --base-path . -o . SimpleStorage.sol
+  // rpc interface: https://playground.open-rpc.org/?schemaUrl=https://raw.githubusercontent.com/ethereum/execution-apis/assembled-spec/openrpc.json&uiSchema%5BappBar%5D%5Bui:splitView%5D=false&uiSchema%5BappBar%5D%5Bui:input%5D=false&uiSchema%5BappBar%5D%5Bui:examplesDropdown%5D=false
+  // http://127.0.0.1:7545
 }
 
 // 运行 mian 函数
-main();
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
