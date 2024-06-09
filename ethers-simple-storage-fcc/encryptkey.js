3e78fcf4975125bf17f2f5e7b2c0afb07889ddb7
icanci <1845666903@qq.com>
2024-06-09 22:07:40 +0800

    encryptkey

---
+++ ethers-simple-storage-fcc/encryptkey.js
@@ -0,0 +1,13 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+require("dotenv").config();
+
+async function main() {}
+
+// 运行 main 函数
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
