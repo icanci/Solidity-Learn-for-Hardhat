52e061a5b3b7104cb40a0a888e475719ac0746d2
icanci <1845666903@qq.com>
2024-06-12 09:39:08 +0800

    sepolia 部署合约成功

--- hardhat-simple-storage-fcc/hardhat.config.js
+++ hardhat-simple-storage-fcc/hardhat.config.js
@@ -15,14 +15,14 @@ const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-// yarn hardhat run scripts/deploy.js --network Sepolia
+// yarn hardhat run scripts/deploy.js --network sepolia
 module.exports = {
     defauleNetwork: "hardhat",
     networks: {
         sepolia: {
             url: SEPOLIA_RPC_URL,
             accounts: [SEPOLIA_PRIVATE_KEY],
-            chainId: 11155420,
+            chainId: 11155111, // HardhatError: HH101: Hardhat was set to use chain id 11155420, but connected to a chain with id 11155111.
         },
     },
     solidity: "0.8.7",
